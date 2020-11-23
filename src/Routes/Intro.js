a3b82777ef1ce8117cb34dd08b9de112dbe4bedd
Keeper991 <nesmile23@gmail.com>
2020-11-24 00:32:19 +0900

    update: style of Intro page

--- src/Routes/Intro.js
+++ src/Routes/Intro.js
@@ -1,5 +1,6 @@
 import styled from "styled-components";
-// import Section from "../Components/Section";
+import RoutePointer from "Components/RoutePointer";
+import bgImage from "Images/bg_intro.jpg";
 
 const Container = styled.div`
   width: 100vw;
@@ -7,23 +8,40 @@ const Container = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
-  background-color: gray;
+  background-color: #95a5a6;
   color: white;
   padding: 0 5em;
+  position: relative;
+  z-index: 1;
+  ::before {
+    position: absolute;
+    content: "";
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-image: url(${bgImage});
+    background-size: cover;
+    opacity: 0.5;
+    z-index: -1;
+  }
 `;
 
 const Title = styled.div`
   font-size: 3rem;
+  text-shadow: 0.1em 0.1em 0.2em #2c3e50;
 `;
 
 const Desc = styled.p`
   margin-top: 1em;
+  text-shadow: 0.1em 0.1em 0.2em #2c3e50;
 `;
 
 const Intro = () => (
   <Container>
+    <RoutePointer id="#" />
     <Title>Sehyeon Park</Title>
-    <Desc>Make a more various world.</Desc>
+    <Desc>Make a more various and funny world.</Desc>
   </Container>
 );
 
